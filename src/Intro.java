e677fa0ac3a2c95904689d880b6e3e9561c8b688
Mikele <michal.minich@o2.pl>
2025-06-05 20:52:44 +0200

    Creating logging-in simulating intro

--- src/Intro.java
+++ src/Intro.java
@@ -1,6 +1,6 @@
 public class Intro {
     public static void intro() throws InterruptedException {
-        System.out.printf("Welcome to your banking app!\nPlease enter your username: ");
+        System.out.print("Welcome to your banking app!\nPlease enter your username: ");
         String username = "Username";
         String password = "********";
         Thread.sleep(1500);
@@ -8,13 +8,13 @@ public class Intro {
             System.out.printf("%c", username.charAt(i));
             Thread.sleep(250);
         }
-        System.out.printf("\nPlease enter your password: ");
+        System.out.print("\nPlease enter your password: ");
         Thread.sleep(1500);
         for (int i = 0; i < password.length(); i++) {
             System.out.printf("%c", password.charAt(i));
             Thread.sleep(250);
         }
-        System.out.printf("\nYou have successfully logged in!");
+        System.out.print("\nYou have successfully logged in!");
         Thread.sleep(1000);
         for (int i=0; i<10; i++){
             System.out.println(" ");
